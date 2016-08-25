d91e34f246812788799f9d2a6725ba510d0c0327
RandyHH <hf_abcd@163.com>
2016-08-25 17:24:20 +0800

    Initial commit

---
+++ hello-world/src/com/randy/demo/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.randy.demo;
+
+/**
+ * Created by Randy on 2016/8/25.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello world!\n" +
+                "I'm Randy. This is my first programming by Intellj IDEA.");
+    }
+}
